b733842ce9dbd0ad0baf33cb47e54606eb4afc16
bastian.langouet@etu.umontpellier.fr <bastian.langouet@etu.umontpellier.fr>
2024-09-13 16:35:06 +0200

    second commit

--- warship.cpp
+++ warship.cpp
@@ -1,6 +1,34 @@
 
 
+class Ship {
+    int length;
+    bool isVertical;
+    int posX;
+    int posY;
 
+    Ship(int length,bool isVertical,int posX,int posY){
+        if(isVertical){
+            if(length+posY >10 || posY<1){
+                 cout << "Please select valid location." << endl;
+                 return 1;
+            }
+        }
+        else{
+            if(length+posX >10 || posX<1){
+                 cout << "Please select valid location." << endl;
+                 return 1;
+        }
+            this.length = length;
+            this.isVertical = isVertical;
+            this.posX = posX;
+            this.posY = posY;
+        }
+    }
+}
+
+class Grille{
+    std::array<std::array<int,10>, 10> Cases; 
+}
 
 int main(){
 
