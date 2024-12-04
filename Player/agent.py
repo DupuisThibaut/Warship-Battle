a6a2ec3928498e6eda2781816efddc2ee4510a38
DupuisThibaut <73234193+DupuisThibaut@users.noreply.github.com>
2024-12-04 18:10:25 +0100

    commit on essaye
    reglage de quelques soucis et en train de bosser sur strategie 3

--- Player/agent.py
+++ Player/agent.py
@@ -13,6 +13,10 @@ class Agent:
         self.haveToGoBack = False
         self.numStrat = numStrat
         self.Candy = [0, 0]
+        self.teacher = []
+        self.chemin = []
+        self.position=0
+        self.back=0
 
     def avancer(self):
         if self.y + 1 < self.gridSize:
@@ -49,7 +53,12 @@ class Agent:
         self.previous = tmp
     
     def GoBack(self):
-        if(self.numStrat==1):
+        self.position=0
+        if self.x == self.initial_Coordinates[0] and self.y == self.initial_Coordinates[1]:
+            print("Est bien revenu")
+            self.previous = [self.x, self.y]
+            self.back=3
+        elif(self.numStrat!=2):
             if self.x > self.initial_Coordinates[0] and self.Candy[0]!=self.x-1:
                 self.gauche()
             elif self.x < self.initial_Coordinates[0] and self.Candy[0]!=self.x+1:
@@ -67,10 +76,6 @@ class Agent:
                 self.gauche()
             elif self.x < self.initial_Coordinates[0] and self.Candy[0]!=self.x+1:
                 self.droite()
-        elif self.x == self.initial_Coordinates[0] and self.y == self.initial_Coordinates[1]:
-            print("Est bien revenu")
-            self.previous = [self.x, self.y]
-            self.haveToGoBack = False
         else:
             self.previous = [self.x, self.y]
             self.x = self.initial_Coordinates[0]
@@ -78,14 +83,20 @@ class Agent:
 
     def whatToDo(self):
         if self.haveToGoBack:
-            self.GoBack()
-
+            if self.back==0:
+                self.GoBack()
+            else:
+                self.back-=1
+                if self.back==0:
+                    self.haveToGoBack=False
         elif self.numStrat == 0:
             self.StratAleat()
         elif self.numStrat == 1:
             self.Stratégie1()
         elif self.numStrat == 2:
             self.Stratégie2()
+        elif self.numStrat == 3:
+            self.Stratégie3()
     
     def StratAleat(self):
         i = random.randint(0, 3)
@@ -122,22 +133,38 @@ class Agent:
         elif self.x < self.Candy[0]:
             self.droite()
     
+    def Stratégie3(self):
+        if(self.position<len(self.chemin)-1):
+            print("i")
+            if self.x<self.chemin[self.position][0]:
+                self.droite()
+                self.position+=1
+            if self.x>self.chemin[self.position][0]:
+                self.gauche()
+                self.position+=1
+            if self.y<self.chemin[self.position][1]:
+                self.avancer()
+                self.position+=1
+            if self.y>self.chemin[self.position][1]:
+                self.reculer()
+                self.position+=1
+    
     def plusCourtChemin(self,x,y,chemin):
         agentX=self.x
         agentY=self.y
         l=[]
         if(chemin==1):
-            while(agentX!=x and agentY!=y):
-                avance=True
-                if agentX==x and agentY==y:
+            avance=True
+            while(agentX!=x or agentY!=y):
+                if agentX==x or agentY==y:
                     if agentY < y:
                         agentY+=1
                         l.append([agentX,agentY])
-                    elif agentX < x:
-                        agentX+=1
-                        l.append([agentX,agentY])
                     elif agentY > y:
                         agentY-=1
+                        l.append([agentX,agentY])
+                    elif agentX < x:
+                        agentX+=1
                         l.append([agentX,agentY])
                     elif agentX > x:
                         agentX-=1
@@ -198,3 +225,39 @@ class Agent:
                     agentX-=1
                     l.append([agentX,agentY])
                     avance=True
+        return l
+
+    def prof(self,eleves):
+        min=self.gridSize*self.gridSize
+        parcours=[]
+        i=0
+        for eleve in eleves:
+            if(eleve.haveToGoBack==False):
+                l=Agent.plusCourtChemin(self,eleve.x,eleve.y,2)
+                if len(l)<min:
+                    min=len(l)
+                    parcours=l
+                    e=i
+            i+=1
+        self.previous[0]=self.x
+        self.previous[1]=self.y
+        parcoursVide=False
+        if parcours==[]:
+            parcours=Agent.plusCourtChemin(self,self.initial_Coordinates[0],self.initial_Coordinates[1],2)
+            parcoursVide=True
+        self.x=parcours[0][0]
+        self.y=parcours[0][1]
+        if(parcoursVide==False):
+            if self.x>0:
+                if eleves[e].x==self.x-1 and eleves[e].y==self.y:
+                    return e
+            if self.x<self.gridSize:
+                if eleves[e].x==self.x+1 and eleves[e].y==self.y:
+                    return e
+            if self.y>0:
+                if eleves[e].y==self.y-1 and eleves[e].x==self.x:
+                    return e
+            if self.y<self.gridSize:
+                if eleves[e].y==self.y+1 and eleves[e].x==self.x:
+                    return e
+        return -1
--- main.py
+++ main.py
@@ -9,8 +9,12 @@ class Main:
     def __init__(self,tailleGrid,agents,teacher):
         self.grid=Environnement(tailleGrid,[random.randint(0,taille-1),random.randint((taille//2)+1,taille-1)])
         self.agents=agents
+        print(self.grid.candy)
         for e in self.agents:
             e.Candy = self.grid.candy
+            if(e.numStrat==3):
+                e.chemin=e.plusCourtChemin(e.Candy[0],e.Candy[1],1)
+                print(e.chemin)
         self.debutJeu=time
         self.teacher=teacher
 
@@ -18,19 +22,25 @@ class Main:
         for e in self.agents:
             e.whatToDo()
             self.grid.displayAgent(e)
+        i=self.teacher.prof(self.agents)
+        if(i!=-1):
+            self.agents[i].haveToGoBack=True
         self.grid.displayAgent(self.teacher)
             
 
 taille=10
-student1=Agent("Yanis",2,0,taille,1)
-student2=Agent("Bastian",1,1,taille,2)
-student3=Agent("Thibaut",2,2,taille,0)
+student1=Agent("Yanis",1,0,taille,1)
+student2=Agent("Bastian",8,0,taille,2)
+student3=Agent("Thibaut",5,0,taille,3)
 students=[student1,student2,student3]
-teacher = Agent("T", (taille // 2)-1, (taille // 2), taille,None)
+teacher = Agent("I", (taille // 2)-1, (taille // 2), taille,None)
 main=Main(taille,students,teacher)
 input_test = "ok"
 x = 30
 start_time = time.time()
+main.updateAgents()
+main.grid.afficher()
+#"""
 while True:
     elapsed_time = time.time() - start_time
     if elapsed_time > x:
@@ -47,3 +57,4 @@ for e in students:
         bestStudent = e
         maxPoint = e.nbPoint
 print(f"Victoire de {bestStudent.nom} : La meilleure stratégie est la {bestStudent.numStrat}")
+#"""
